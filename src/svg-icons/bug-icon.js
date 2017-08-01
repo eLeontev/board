17d00679228d6e2668736dc8fea44ccacdbebf77
eLeontev <joonior199@gmail.com>
2017-08-02 00:03:32 +0300

    remove unusable property for svg icons

--- src/svg-icons/bug-icon.js
+++ src/svg-icons/bug-icon.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const BugIcon = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="16" height="16" viewBox="0 0 16 16">
+    <svg width="16" height="16" viewBox="0 0 16 16">
         <image id="Layer_0" data-name="Layer 0" x="1" y="1" width="14" height="14" xlinkHref="data:img/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA4AAAAOCAMAAAAolt3jAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAPFBMVEW1tbWLi4vT09P7+/v////Pz8/Z2dn88vH5+fnW1tbU1NSzs7OwsLBwcHBycnKBgYHbb2XQRDd7e3sAAADayQaxAAAADXRSTlMi7u7u3e7u7u7u7iIiv49gPQAAAAFiS0dEEwy7XJYAAAAJcEhZcwAACxIAAAsSAdLdfvwAAAAHdElNRQfhCAEQLQsy3m40AAAAVklEQVQI13XP2QrAIAxE0Ylaly5R+/8f26FWBaH3RQ+IJIBc2hNAsrGuZbJANzfyCrWTluQRSq0l8NJI0YP17Y/L4+WrXiT9ZFIOuX+DxHQLDtG5wvkAlHoGd1P6l0sAAAAASUVORK5CYII="/>
     </svg>
 );
--- src/svg-icons/subTask-icon.js
+++ src/svg-icons/subTask-icon.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const SubTaskIcon = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="16" height="16" viewBox="0 0 16 16">
+    <svg width="16" height="16" viewBox="0 0 16 16">
         <image id="Layer_0" data-name="Layer 0" x="1" y="1" width="14" height="14" xlinkHref="data:img/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA4AAAAOCAMAAAAolt3jAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAWlBMVEW1tbWLi4vT09P7+/v////Pz8++q5rFtaXZ2dn39PLFs6TMva/d08r5+fnW1tazs7OwsLBwcHBycnKBgYGBXDuBWzqDXj6HY0OPbVCDXj2LaUqObE57e3sAAAAKF8lgAAAAEXRSTlMi7u7u3e7u7u7u7u7u7u4iIiHWCakAAAABYktHRB3rA3GRAAAACXBIWXMAAAsSAAALEgHS3X78AAAAB3RJTUUH4QgBEC0eXwOK3wAAAGdJREFUCNdNz1kOgDAIBFCwqHVXtHW9/zlFiNb5e4FMAAAnfoMAOGeOLNmMwHmxhFiqPQO7taJqUzoh7RQo2vrDQ6ZnYr0usUkkWSbr+2h9xjYE6zNKtK8TeqX29fw7susvhAE5vTDeL/sIIQLV2s4AAAAASUVORK5CYII="/>
     </svg>
 );
